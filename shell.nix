5733047eba791ab69917100302c97b842b2d2bdc
amatgil <amatgilvinyes@gmail.com>
2026-04-25 03:40:25 +0200

    Fix cmake for nix (FINALLY OH MY GOD)

--- shell.nix
+++ shell.nix
@@ -11,11 +11,20 @@ let
     xorg.libX11
     xorg.libX11.dev
     xorg.libXcursor
+    xorg.libXcursor.dev
     xorg.libXi
+    xorg.libXi.dev
     xorg.libXinerama
+    xorg.libXinerama.dev
     xorg.libXrandr
-    emscripten # web support
+    xorg.libXrandr.dev
+    libxrandr
+    libxrandr.dev
+    libxinerama
+    libxinerama.dev
+    #emscripten # web support
     clang
+    pkg-config
 
     raylib
   ];
@@ -25,7 +34,12 @@ pkgs.mkShell {
   nativeBuildInputs = packages;
   buildInputs = packages;
   env = {
+    X11_X11_INCLUDE_PATH = pkgs.xorg.libX11.dev;
+    X11_LIB_PATH = pkgs.xorg.libX11.dev;
     LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
     LD_LIBRARY_PATH = "${lib.makeLibraryPath packages}";
   };
+  shellHook = ''
+    export CMAKE_PREFIX_PATH=${pkgs.raylib}/lib/cmake:$CMAKE_PREFIX_PATH
+  '';
 }
