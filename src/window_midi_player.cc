d6db28e67addd7b1f6d187b8322b74b7b216064d
bernatcuixart <bernat.cuixart@estudiantat.upc.edu>
2026-04-25 04:45:04 +0200

    Started work on midi player

--- src/window_midi_player.cc
+++ src/window_midi_player.cc
@@ -2,8 +2,10 @@
 
 Camera2D _cam = { 0 };
 
-Sheet playing_sheet;
 float playing_time = 0.0f;
+
+int notes_count = 0;
+midi_player_note* notes = nullptr;
 
 void update_midi_playback(const float deltaTime)
 {
@@ -12,19 +14,40 @@ void update_midi_playback(const float deltaTime)
 
 void play_midi(const Sheet& sheet) 
 {
-	playing_sheet = sheet;
 	playing_time = 0.0f;
+
+	notes_count = sheet.pitch.size();
+	if (notes != nullptr) free(notes);
+
+    notes = (midi_player_note*)malloc(sizeof(midi_player_note) * notes_count);
+	for (int i = 0; i < notes_count; ++i)
+    {
+        float width = 1.0f;
+        float height = (float)sheet.durations[i] * VERTICAL_SCALE;
+        float x = (float)sheet.pitch[i] - 44; // TODO: posar de -44 a 43
+        float y = -(float)sheet.timestamps_start[i] * VERTICAL_SCALE - height;
+
+        notes[i].position = { x, y };
+        notes[i].size = { width, height };
+
+		printf("Note %d: position=(%f, %f), size=(%f, %f)\n", i, x, y, width, height);
+    }
 }
 
 void draw_midi_player_screen()
 {
-    _cam.target = { 0, 0 };
-    //_cam.offset = { width / 2.0f, height / 2.0f }; // Screen center
-    _cam.rotation = 0.0f;
-    //_cam.zoom = (float)height / MAP_COORD_SIZE;
+	_cam.target = { 0, -playing_time * VERTICAL_SCALE };
+	_cam.offset = (Vector2){ GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f };
+	_cam.rotation = 0.0f;
+	_cam.zoom = (float)GetScreenWidth() / 88.0f;
 
     BeginMode2D(_cam);
     ClearBackground(COLOR_BACKGROUND);
+
+    for (int i = 0; i < notes_count; ++i)
+    {
+        DrawRectangleV(notes[i].position, notes[i].size, COLOR_NOTE);
+	}
 
     EndMode2D();
 }
--- src/window_midi_player.hh
+++ src/window_midi_player.hh
@@ -1,12 +1,25 @@
 #ifndef WINDOW_MIDI_PLAYER_HH
 #define WINDOW_MIDI_PLAYER_HH
 
+#include <cstdlib>
+#include <stdio.h>
+
 #include "sheet.hh"
 #include "raylib.h"
 
 #define COLOR_BACKGROUND (Color){ 88, 8, 94, 255 }
+#define COLOR_NOTE (Color){ 212, 16, 230, 255 }
+
+#define VERTICAL_SCALE 10.0f
+
+typedef struct {
+	Vector2 position;
+	Vector2 size;
+} midi_player_note;
 
 void play_midi(const Sheet& sheet);
+
+void update_midi_playback(const float deltaTime);
 
 void draw_midi_player_screen();
 
