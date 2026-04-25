1732cfe759e6dfdf48d34bcfb81710cba437966f
bernatcuixart <bernat.cuixart@estudiantat.upc.edu>
2026-04-25 20:20:45 +0200

    Commit and whatnot

--- src/window_midi_editor.hh
+++ src/window_midi_editor.hh
@@ -1,10 +1,8 @@
 #pragma once
 #include "sheet.hh"
 
-#define COLOR_BACKGROUND                                                       \
-    (Color) { 88, 8, 94, 255 }
-#define COLOR_BACKGROUND_DARK                                                  \
-    (Color) { 88, 8, 94, 255 }
+#define COLOR_BACKGROUND (Color) { 88, 8, 94, 255 }
+#define COLOR_BACKGROUND_DARK (Color) { 88, 8, 94, 255 }
 
 void drawGrid(int xoffset, int yoffset, int col_width, int row_width);
 
