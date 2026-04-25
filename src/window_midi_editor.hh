d6856ed86d05e3cc7205a7f1a20893c67084c162
Guillem Baldi <guillembaldi@gmail.com>
2026-04-25 20:10:30 +0200

    Stuffs for Bernat (ew)

--- src/window_midi_editor.hh
+++ src/window_midi_editor.hh
@@ -1,8 +1,13 @@
 #pragma once
 #include "sheet.hh"
 
-void drawGrid(int xoffset, int yoffset, int col_width,int row_width);
+#define COLOR_BACKGROUND                                                       \
+    (Color) { 88, 8, 94, 255 }
+#define COLOR_BACKGROUND_DARK                                                  \
+    (Color) { 88, 8, 94, 255 }
 
-void drawSoundTimeline(Sheet& sheet);
+void drawGrid(int xoffset, int yoffset, int col_width, int row_width);
+
+void drawSoundTimeline(Sheet &sheet);
 
 void initEditor();
