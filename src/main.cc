fcc198a10cf43be9ee8710c86740fc17684697f9
bernatcuixart <bernat.cuixart@estudiantat.upc.edu>
2026-04-25 02:38:12 +0200

    Made window... sort of

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#include "raylib.h"
 
 #include "sheet.cc"
 
-int main() {
-  printf("Haii\n");
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+
+int main(int argc, char* argv[]) 
+{
+    if (argc == 3)
+    {
+        int wdth = atoi(argv[1]);
+        int hght = atoi(argv[2]);
+
+        InitWindow(wdth, hght, "Midi");
+    }
+    else
+    {
+        InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Midi");
+    }
+
+    SetExitKey(KEY_NULL);
+
+    InitAudioDevice();
+    SetTargetFPS(60);
+
+    float deltaTime = 0;
+    while (!WindowShouldClose())
+    {
+        deltaTime = GetFrameTime();
+
+        // UPDATE APP
+    }
+
+    CloseAudioDevice();
+    CloseWindow();
+
+    return 0;
 }  
